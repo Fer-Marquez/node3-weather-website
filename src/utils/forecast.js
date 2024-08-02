d97a422ddfdf6a15dd049fda6e1d9dc36e322b37
Fer Marquez <fernanda.marquezgonzlez@gmail.com>
2024-08-02 10:24:24 -0600

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humedity + "%")
         }
     })
 }
